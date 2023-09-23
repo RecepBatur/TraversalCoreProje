39a092fffe13463fc316200fa2a958984b1c116e
Recep Batur <recep_btr44@hotmail.com>
2023-09-23 15:40:54 +0300

    Context class added. And database connection added.

--- EntityLayer/Concrete/NewsLetter.cs
+++ EntityLayer/Concrete/NewsLetter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace EntityLayer.Concrete
 {
     public class NewsLetter
     {
+        [Key]
         public int NewsLetterId { get; set; }
         public string Mail { get; set; }
     }
