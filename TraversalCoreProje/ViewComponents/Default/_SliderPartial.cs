d3bf44219811d971539277cccf7ea24911a34526
Recep Batur <recep_btr44@hotmail.com>
2023-09-25 01:16:45 +0300

    ViewComponents added.

---
+++ TraversalCoreProje/ViewComponents/Default/_SliderPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.Default
+{
+    public class _SliderPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
