bc1b077869e115d9917103cab1d1ef20cf1b739b
Recep Batur <recep_btr44@hotmail.com>
2023-09-26 01:46:15 +0300

    Layout regulation.

---
+++ TraversalCoreProje/Controllers/DestinationController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Controllers
+{
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult DestinaonDetails(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DestinaonDetails(Destination p)
+        {
+            return View();
+        }
+    }
+}
