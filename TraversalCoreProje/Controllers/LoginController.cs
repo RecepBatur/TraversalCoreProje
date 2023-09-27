8460675f72c7d47e4841d2fd4c4ca68aaa2dde04
Recep Batur <recep_btr44@hotmail.com>
2023-09-27 23:59:55 +0300

    Login and Register theme added.

---
+++ TraversalCoreProje/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Controllers
+{
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public IActionResult SignUp()
+        //{
+        //    return View();
+        //}
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
