dbcc66d8445f64c946bc5672a09053d5e9832959
Recep Batur <recep_btr44@hotmail.com>
2023-09-28 16:09:50 +0300

    Areas and Member Layout regulation.

---
+++ TraversalCoreProje/Areas/Member/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Areas.Member.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- TraversalCoreProje/Program.cs
+++ TraversalCoreProje/Program.cs
@@ -45,4 +45,13 @@ app.MapControllerRoute(
 	name: "default",
 	pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
+
+
 app.Run();
