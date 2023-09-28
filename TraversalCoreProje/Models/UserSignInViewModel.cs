8d4630ecd0b4bb015675facc4fbc7f50fa0b9a6e
Recep Batur <recep_btr44@hotmail.com>
2023-09-29 00:32:01 +0300

    Identity with login transaction.

---
+++ TraversalCoreProje/Models/UserSignInViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TraversalCoreProje.Models
+{
+    public class UserSignInViewModel
+    {
+        [Required(ErrorMessage = "Lütfen kullanıcı adını giriniz")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifrenizi giriniz")]
+        public string Password { get; set; }
+    }
+}
