d1e052ca0174edc222b558c2629bd3ab5ff66044
Recep Batur <recep_btr44@hotmail.com>
2023-09-30 18:59:53 +0300

    Member profile regulation.

---
+++ TraversalCoreProje/Areas/Member/Controllers/DashboardController.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class DashboardController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public DashboardController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.userName = values.Name + " " + values.Surname;
+            ViewBag.Image = values.ImageUrl;
+            return View();
+        }
+    }
+}
