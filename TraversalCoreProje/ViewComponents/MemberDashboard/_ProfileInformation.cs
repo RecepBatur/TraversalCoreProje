a016cc487b33108be17b71046c293d03fd2c0406
Recep Batur <recep_btr44@hotmail.com>
2023-09-30 19:35:35 +0300

    Dashboard profile information listed.

---
+++ TraversalCoreProje/ViewComponents/MemberDashboard/_ProfileInformation.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.MemberDashboard
+{
+    public class _ProfileInformation : ViewComponent
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public _ProfileInformation(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.memberName = values.Name + " " + values.Surname;
+            ViewBag.memberPhone = values.PhoneNumber;
+            ViewBag.memberEmail = values.Email;
+            return View();
+        }
+    }
+}
