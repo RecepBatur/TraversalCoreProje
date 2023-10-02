50e17979d2666e6809868390607f432cd0f1bff6
Recep Batur <recep_btr44@hotmail.com>
2023-10-02 18:17:55 +0300

    AdminLayout partialview regulation.

---
+++ TraversalCoreProje/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Controllers
+{
+    public class AdminController : Controller
+    {
+        public PartialViewResult PartialHeader()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialAppBrandDemo()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialMenu()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScripts()
+        {
+            return PartialView();
+        }
+    }
+}
