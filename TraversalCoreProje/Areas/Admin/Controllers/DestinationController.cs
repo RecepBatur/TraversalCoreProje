d88c5964d2368931635df572aa24826aabce11ef
Recep Batur <recep_btr44@hotmail.com>
2023-10-02 12:21:54 +0300

    Admin new destination added.

--- TraversalCoreProje/Areas/Admin/Controllers/DestinationController.cs
+++ TraversalCoreProje/Areas/Admin/Controllers/DestinationController.cs
@@ -1,13 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProje.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class DestinationController : Controller
     {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+
         public IActionResult Index()
         {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddDestination()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult AddDestination(Destination destination)
+        {
+            destinationManager.TAdd(destination);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteDestination(int id)
+        {
+           var values = destinationManager.TGetById(id);
+           destinationManager.TDelete(values);
+           return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateDestination(int id)
+        {
+            var values = destinationManager.TGetById(id);
+            return View();
+        }
+        [HttpPost]
+        public IActionResult UpdateDestination(Destination destination)
+        {
+            destinationManager.TUpdate(destination);
+            return RedirectToAction("Index");
         }
     }
 }
