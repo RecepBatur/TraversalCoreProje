0932fcaa26c993e79f2c260e66d62fd16bcfd828
Recep Batur <recep_btr44@hotmail.com>
2023-10-04 00:13:57 +0300

    Admin panel user delete transactions.

--- TraversalCoreProje/Areas/Admin/Controllers/UserController.cs
+++ TraversalCoreProje/Areas/Admin/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProje.Areas.Admin.Controllers
@@ -17,6 +18,34 @@ namespace TraversalCoreProje.Areas.Admin.Controllers
         {
             var values = _appUserService.TGetList();
             return View(values);
+        }
+        public IActionResult DeleteUser(int id)
+        {
+            var values = _appUserService.TGetById(id);
+            _appUserService.TDelete(values);
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult UpdateUser(int id)
+        {
+            var values = _appUserService.TGetById(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult UpdateUser(AppUser appUser)
+        {
+            _appUserService.TUpdate(appUser);
+            return RedirectToAction("Index");
+        }
+        public IActionResult CommentUser(int id)
+        {
+            _appUserService.TGetList();
+            return View();
+        }
+        public IActionResult ReservationUser(int id)
+        {
+            _appUserService.TGetList();
+            return View();
         }
     }
 }
