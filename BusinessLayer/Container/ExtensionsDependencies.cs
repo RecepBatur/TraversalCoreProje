275de016a1142809958d66ab81a1e4dcf4464d7f
Recep Batur <recep_btr44@hotmail.com>
2023-10-04 23:48:35 +0300

    Admin panel Guide getlist.

--- BusinessLayer/Container/ExtensionsDependencies.cs
+++ BusinessLayer/Container/ExtensionsDependencies.cs
@@ -24,6 +24,8 @@ namespace BusinessLayer.Container
             services.AddScoped<IAppUserDal, EfAppUserDal>();
             services.AddScoped<IReservationService, ReservationManager>();
             services.AddScoped<IReservationDal, EfReservationDal>();
+            services.AddScoped<IGuideService, GuideManager>();
+            services.AddScoped<IGuideDal, EfGuideDal>();
         }
     }
 }
---
+++ TraversalCoreProje/Areas/Admin/Controllers/GuideController.cs
@@ -0,0 +1,53 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class GuideController : Controller
+    {
+        private readonly IGuideService _guideService;
+
+        public GuideController(IGuideService guideService)
+        {
+            _guideService = guideService;
+        }
+        public IActionResult Index()
+        {
+            var values = _guideService.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddGuide()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddGuide(Guide guide)
+        {
+            _guideService.TAdd(guide);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateGuide(int id)
+        {
+            var values = _guideService.TGetById(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult UpdateGuide(Guide guide)
+        {
+            _guideService.TUpdate(guide);
+            return RedirectToAction("Index");
+        }
+        public IActionResult ChangeToTrue(int id)
+        {
+            return RedirectToAction("Index");
+        }
+        public IActionResult ChangeToFalse(int id)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
