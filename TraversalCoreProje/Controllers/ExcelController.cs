3721a1ebfb44499c9987ff4e7ad17e8b439d0f49
Recep Batur <recep_btr44@hotmail.com>
2023-10-05 23:35:14 +0300

    Excel export transactions.

---
+++ TraversalCoreProje/Controllers/ExcelController.cs
@@ -0,0 +1,81 @@
+﻿using ClosedXML.Excel;
+using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+using TraversalCoreProje.Models;
+
+namespace TraversalCoreProje.Controllers
+{
+    public class ExcelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public List<DestinationModel> DestinationList()
+        {
+            List<DestinationModel> destinationModels = new List<DestinationModel>();
+            using (var c = new Context())
+            {
+                destinationModels = c.Destinations.Select(x => new DestinationModel
+                {
+                    City = x.City,
+                    Capacity = x.Capacity,
+                    DayNight = x.DayNight,
+                    Price = x.Price,
+                }).ToList();
+
+                return destinationModels;
+            }
+        }
+        public IActionResult StaticExcelReport()
+        {
+
+            ExcelPackage excel = new ExcelPackage();
+            var workSheet = excel.Workbook.Worksheets.Add("Sayfa1");
+            workSheet.Cells[1, 1].Value = "Rota";
+            workSheet.Cells[1, 2].Value = "Rehber";
+            workSheet.Cells[1, 3].Value = "Kontenjan";
+
+
+            workSheet.Cells[2, 1].Value = "Gürcistan Batum Turu";
+            workSheet.Cells[2, 2].Value = "Kadir Yıldız";
+            workSheet.Cells[2, 3].Value = "50";
+
+            workSheet.Cells[3, 1].Value = "Sırbistan - Makedonya Turu";
+            workSheet.Cells[3, 2].Value = "Zeynep Öztürk";
+            workSheet.Cells[3, 3].Value = "35";
+
+            var bytes = excel.GetAsByteArray();
+            return File(bytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "dosya2.xlsx");
+        }
+        public IActionResult DestinationExcelReport()
+        {
+            using (var workBook = new XLWorkbook())
+            {
+                var workSheet = workBook.Worksheets.Add("Tur Listesi");
+                workSheet.Cell(1, 1).Value = "Şehir";
+                workSheet.Cell(1, 2).Value = "Konaklama Süresi";
+                workSheet.Cell(1, 3).Value = "Fiyat";
+                workSheet.Cell(1, 4).Value = "Kapasite";
+
+                int rowCount = 2;
+                foreach (var item in DestinationList())
+                {
+                    workSheet.Cell(rowCount, 1).Value = item.City;
+                    workSheet.Cell(rowCount, 2).Value = item.DayNight;
+                    workSheet.Cell(rowCount, 3).Value = item.Price;
+                    workSheet.Cell(rowCount, 4).Value = item.Capacity;
+                    rowCount++;
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workBook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Yeni Liste.xlsx");
+                }
+            }
+        }
+    }
+}
