8fc5b4c444ff3f123ac54dc955e31d097a32f7e1
Recep Batur <recep_btr44@hotmail.com>
2023-10-07 16:48:33 +0300

    Mail send transactions.

---
+++ TraversalCoreProje/Areas/Admin/Controllers/MailController.cs
@@ -0,0 +1,41 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using TraversalCoreProje.Models;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(MailRequest mailRequest)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("Admin", "recep.btr44@gmail.com");
+            mimeMessage.From.Add(mailboxAddressFrom);
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", mailRequest.ReceiverMail);
+
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = mailRequest.Body;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = mailRequest.Subject;
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Connect("smtp.gmail.com", 587, false);
+            smtpClient.Authenticate("recep.btr44@gmail.com", "kdısyhsalmwunsbchşd");
+            smtpClient.Send(mimeMessage);
+            smtpClient.Disconnect(true);
+
+            return View();
+        }
+    }
+}
