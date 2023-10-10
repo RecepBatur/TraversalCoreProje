701587765605cfdf2303676158744858d9c21729
Recep Batur <recep_btr44@hotmail.com>
2023-10-10 22:31:12 +0300

    Admin city ajax getlist.

---
+++ TraversalCoreProje/Areas/Admin/Controllers/CityController.cs
@@ -0,0 +1,42 @@
+﻿using DocumentFormat.OpenXml.Bibliography;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TraversalCoreProje.Models;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CityList()
+        {
+            var jsonCity = JsonConvert.SerializeObject(cities);
+            return Json(jsonCity);
+        }
+        public static List<CityClass> cities = new List<CityClass>
+        {
+            new CityClass
+            {
+                CityId = 1,
+                CityName = "Üsküp",
+                CityCountry = "Makedonya"
+            },
+            new CityClass
+            {
+                CityId = 2,
+                CityName = "Roma",
+                CityCountry = "İtalya"
+            },
+            new CityClass
+            {
+                CityId = 3,
+                CityName = "Londra",
+                CityCountry = "İngiltere"
+            }
+        };
+    }
+}
---
+++ TraversalCoreProje/Models/CityClass.cs
@@ -0,0 +1,9 @@
+﻿namespace TraversalCoreProje.Models
+{
+    public class CityClass
+    {
+        public int CityId { get; set; }
+        public string CityName { get; set; }
+        public string CityCountry { get; set; }
+    }
+}
