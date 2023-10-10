9cccada52a3f9a6ac0289de3dad2c534d89d0521
Recep Batur <recep_btr44@hotmail.com>
2023-10-11 00:30:31 +0300

    Admin city added and getbyid ajax transactions.

--- TraversalCoreProje/Areas/Admin/Controllers/CityController.cs
+++ TraversalCoreProje/Areas/Admin/Controllers/CityController.cs
@@ -1,4 +1,6 @@
-﻿using DocumentFormat.OpenXml.Bibliography;
+﻿using BusinessLayer.Abstract;
+using DocumentFormat.OpenXml.Bibliography;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using TraversalCoreProje.Models;
@@ -8,35 +10,35 @@ namespace TraversalCoreProje.Areas.Admin.Controllers
     [Area("Admin")]
     public class CityController : Controller
     {
+        private readonly IDestinationService _destinationService;
+
+        public CityController(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
         public IActionResult Index()
         {
             return View();
         }
         public IActionResult CityList()
         {
-            var jsonCity = JsonConvert.SerializeObject(cities);
+            var jsonCity = JsonConvert.SerializeObject(_destinationService.TGetList());
             return Json(jsonCity);
         }
-        public static List<CityClass> cities = new List<CityClass>
+        [HttpPost]
+        public IActionResult AddCityDestination(Destination destination)
         {
-            new CityClass
-            {
-                CityId = 1,
-                CityName = "Üsküp",
-                CityCountry = "Makedonya"
-            },
-            new CityClass
-            {
-                CityId = 2,
-                CityName = "Roma",
-                CityCountry = "İtalya"
-            },
-            new CityClass
-            {
-                CityId = 3,
-                CityName = "Londra",
-                CityCountry = "İngiltere"
-            }
-        };
+            destination.Status = true;
+            _destinationService.TAdd(destination);
+            var values = JsonConvert.SerializeObject(destination);
+            return Json(values);
+        }
+        public IActionResult GetById(int DestinationId)
+        {
+            var values = _destinationService.TGetById(DestinationId);
+            var jsonValues = JsonConvert.SerializeObject(values);
+            return Json(jsonValues);
+        }
+
     }
 }
