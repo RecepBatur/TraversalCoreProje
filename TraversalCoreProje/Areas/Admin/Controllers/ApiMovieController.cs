e55838816173909c4b61d8c2c948601746e517eb
Recep Batur <recep_btr44@hotmail.com>
2023-10-14 00:10:29 +0300

    Rapid Api Imdb Top 100 Movies api getlist.

---
+++ TraversalCoreProje/Areas/Admin/Controllers/ApiMovieController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TraversalCoreProje.Areas.Admin.Models;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class ApiMovieController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<ApiMovieViewModel> apiMovieViewModels = new List<ApiMovieViewModel>();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "ab6384d5fbmshd8ab6954dc27233p15fc23jsn2326e9c00b1e" },
+        { "X-RapidAPI-Host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                apiMovieViewModels = JsonConvert.DeserializeObject<List<ApiMovieViewModel>>(body);
+                return View(apiMovieViewModels);
+            }
+        }
+    }
+}
---
+++ TraversalCoreProje/Areas/Admin/Models/ApiMovieViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TraversalCoreProje.Areas.Admin.Models
+{
+    public class ApiMovieViewModel
+    {
+        public int rank { get; set; }
+        public string title { get; set; }
+        public string rating { get; set; }
+        public int year { get; set; }
+        public string trailer { get; set; }
+
+    }
+}
