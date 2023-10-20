21d0e390bfbdbb71fd3d67c5eb8b6bcb672a797f
Recep Batur <recep_btr44@hotmail.com>
2023-10-21 00:56:25 +0300

    Destination Details regulation.

--- TraversalCoreProje/Controllers/DestinationController.cs
+++ TraversalCoreProje/Controllers/DestinationController.cs
@@ -1,10 +1,12 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProje.Controllers
 {
+    [AllowAnonymous]
     public class DestinationController : Controller
     {
         DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
---
+++ TraversalCoreProje/ViewComponents/Destination/_RandomGuide.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.Destination
+{
+    public class _RandomGuide : ViewComponent
+    {
+        private readonly IGuideService _guideService;
+
+        public _RandomGuide(IGuideService guideService)
+        {
+            _guideService = guideService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var values = _guideService.TGetById(5);
+            return View(values);
+        }
+    }
+}
