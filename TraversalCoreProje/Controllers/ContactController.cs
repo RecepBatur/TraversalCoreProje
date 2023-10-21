3e224d227d977484df7ddf9fb5100cb65d12c833
Recep Batur <recep_btr44@hotmail.com>
2023-10-21 19:52:48 +0300

    UI Guide getlist.

---
+++ TraversalCoreProje/Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Controllers
+{
+    [AllowAnonymous]
+    public class ContactController : Controller
+    {
+        ContactUsManager contactUsManager = new ContactUsManager(new EfContactUsDal());
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TraversalCoreProje/Controllers/GuideController.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Controllers
+{
+    [AllowAnonymous]
+    public class GuideController : Controller
+    {
+        GuideManager guideManager = new GuideManager(new EfGuideDal());
+        public IActionResult Index()
+        {
+            var values = guideManager.TGetList();
+            return View(values);
+        }
+    }
+}
